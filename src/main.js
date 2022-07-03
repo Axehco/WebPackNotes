5c53a1e65518e5615ea70c4ee4e5574c9dd76489
Axehco <mbozhong@outlook.com>
2022-07-03 21:14:14 +0800

    44_codesplit多入口按需加载

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import {sum} from './math'
+// 按需加载，下面注释掉
+// import count from './count'
 
 console.log("hello main");
 console.log(sum(5, 9));
+
+document.getElementById('btn').onclick = function () {
+  // console.log(count(1, 89));
+  import('./count')
+  .then(res => console.log('模块加载成功了', res.default(1, 89)))
+  .catch((err) => console.log('模块加载失败', err))
+}
